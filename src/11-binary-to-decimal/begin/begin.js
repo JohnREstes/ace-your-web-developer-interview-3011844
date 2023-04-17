5a0f98831ef7dd4a1c9996264817f4c34b353765
John R. Estes <John.R.Estes@icloud.com>
2023-04-17 12:11:01 -0500

    Binary to Dec

--- src/11-binary-to-decimal/begin/begin.js
+++ src/11-binary-to-decimal/begin/begin.js
@@ -1,7 +1,29 @@
-function binaryToDecimal(binary) {}
+const alpha = /[a-z]/g
+const notBinary = /[2-9]/g
+
+function binaryToDecimal(binary) {
+    let valid = true
+    let binaryArray = binary.split('');
+    binaryArray.forEach(item => {
+        if(alpha.test(item) || notBinary.test(item)) valid = false
+    });
+    if(valid){
+            let count = 0;
+    let postion = 1;
+    while(binaryArray.length > 0){
+        count += binaryArray.pop() * postion;
+        postion = postion * 2;
+    }
+    return count
+    } else return `${binary} is not binary`
+}
 
 console.log(binaryToDecimal("101010")); // 42
 console.log(binaryToDecimal("101100000")); // 352
 console.log(binaryToDecimal("1")); // 1
 console.log(binaryToDecimal("13298436")); // "13298436 is not a valid binary string"
 console.log(binaryToDecimal("abc")); // "abc is not a valid binary string"
+
+function binaryConverter(valueArray){
+
+}
