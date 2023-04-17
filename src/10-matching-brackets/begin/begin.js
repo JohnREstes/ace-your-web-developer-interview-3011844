36d3e5e8d78f0c5edae75fa415e840d6bf79cba3
John R. Estes <John.R.Estes@icloud.com>
2023-04-16 19:53:21 -0500

    brackets

--- src/10-matching-brackets/begin/begin.js
+++ src/10-matching-brackets/begin/begin.js
@@ -1,14 +1,22 @@
-function matchingBrackets(str) {}
-
-console.log(`${matchingBrackets("[hello][world]")} should be true`); // true
-console.log(`${matchingBrackets("[hello]][world]")} should be false`); // false
+console.log(`${matchingBrackets("[hello][world]")} should be false`); // false
 console.log(`${matchingBrackets("[hello][world][]")} should be true`); // true
 console.log(`${matchingBrackets("]hello]][world]")} should be false`); // false
 console.log(`${matchingBrackets("[[[as;dfi")} should be false`); // false
+console.log(`${matchingBrackets("[hello][world]")} should be true`); // true
 
-const openBracket = /[[]/g;
-const closeBracket = /[\]]/g
+const bracketArray = [];
 
 function matchingBrackets(value){
-
+    let counter = 0;
+    let bracketArray = value.split('');
+    for(elem of bracketArray){
+        if(elem === '['){
+            counter++;
+        }else if(counter > 0 && elem === ']'){
+            counter = -1;
+        }
+    }
+    
+    if(counter === 0) return true;
+    else return false
 }
